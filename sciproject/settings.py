d700a2215e37dba5ee4c94d4a727701fecf880cd
MasterTos <MasterTos@yahoo.com>
2018-09-27 14:56:41 +0700

    update static and media path

--- sciproject/settings.py
+++ sciproject/settings.py
@@ -151,15 +151,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'static'))
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    #'/var/www/static/',
-]
+
 #STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), '/desktop/sciproject/static/')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = 'media/'
+MEDIA_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'media'))
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
