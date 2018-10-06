5cc5b4b1f026efb18c5bf56018aa1dbd443fdaf1
MasterTos <MasterTos@yahoo.com>
2018-10-06 18:39:19 +0700

    change db engine

--- sciproject/settings.py
+++ sciproject/settings.py
@@ -104,13 +104,13 @@ WSGI_APPLICATION = 'sciproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'postgres',
-        # 'USER': 'postgres',
-        # 'HOST': 'db_exam',
-        # 'PORT': 5432,
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db_exam',
+        'PORT': 5432,
 
     }
 }
